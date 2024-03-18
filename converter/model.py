7a23909f96998b840a571bca6bf2ddd713021d1b
Yiping Liu <yiping.liu@rwth-aachen.de>
2024-03-18 14:44:22 +0100

    thermopy syn

--- converter/model.py
+++ converter/model.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 import converter.methods as pm
 
 import thermopy as tp
-from thermopy.database.solid_stoichio import solids
+from thermopy.database import Db
 from thermopy.utilities.functions import get_charge_number, calculate_ionic_strength
 
 
