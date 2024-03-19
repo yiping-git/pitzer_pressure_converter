ed09abbe4018eafc725676cfaa5c1d06150022e7
Yiping Liu <yiping.liu@rwth-aachen.de>
2024-03-19 17:12:43 +0100

    format adjust

--- converter/model_archer.py
+++ converter/model_archer.py
@@ -75,202 +75,202 @@ class PressureConverter:
         self.alpha2 = 2.5
 
 
-def get_salt_data(self):
-    data = DbStoichio(compound=self.salt)
-    return data
+    def get_salt_data(self):
+        data = DbStoichio(compound=self.salt)
+        return data
 
 
-def get_molalities(self, m):
-    cations = self.salt_data.cations
-    anions = self.salt_data.anions
-    neutral = self.salt_data.neutrals
+    def get_molalities(self, m):
+        cations = self.salt_data.cations
+        anions = self.salt_data.anions
+        neutral = self.salt_data.neutrals
 
-    molalities = {}
+        molalities = {}
 
-    for cation in cations:
-        molalities[cation] = m * self.salt_data.coefficients[cation]
-    for anion in anions:
-        molalities[anion] = m * self.salt_data.coefficients[anion]
+        for cation in cations:
+            molalities[cation] = m * self.salt_data.coefficients[cation]
+        for anion in anions:
+            molalities[anion] = m * self.salt_data.coefficients[anion]
 
-    return molalities
-
-
-def get_ionic_strength(self, m):
-    """
-    Calculating ionic strength, can be molality based or mole fraction based.
-    :param m: moalilty of the electrolyte.
-    :return: ionic strength.
-    """
-    i = calculate_ionic_strength(self.get_molalities(m))
-    return i
+        return molalities
 
 
-@staticmethod
-def get_a_phi(T, P):
-    return a_phi(T=T, P=P, method="Fernandez")
+    def get_ionic_strength(self, m):
+        """
+        Calculating ionic strength, can be molality based or mole fraction based.
+        :param m: moalilty of the electrolyte.
+        :return: ionic strength.
+        """
+        i = calculate_ionic_strength(self.get_molalities(m))
+        return i
 
 
-@staticmethod
-def get_a_l(T, P):
-    return a_h(T=T, P=P, method="Fernandez")
+    @staticmethod
+    def get_a_phi(T, P):
+        return a_phi(T=T, P=P, method="Fernandez")
 
 
-@staticmethod
-def get_a_j(T, P):
-    return a_j(T=T, P=P, method="Fernandez")
+    @staticmethod
+    def get_a_l(T, P):
+        return a_h(T=T, P=P, method="Fernandez")
 
 
-def h(self, i):
-    b = 1.2
-    return np.log(1 + b * np.sqrt(i)) / (2 * b)
+    @staticmethod
+    def get_a_j(T, P):
+        return a_j(T=T, P=P, method="Fernandez")
 
 
-def osmotic_coefficient_difference(self):
-    """
-    Returns:the difference between φ(P2) and φ(P1), that is, φ(P2) - φ(P1).
-    """
-    alpha = self.alpha
-    cation = self.salt_data.cations[0]
-    anion = self.salt_data.cations[0]
-    z_c = get_charge_number(cation)
-    z_a = get_charge_number(anion)
-
-    nu_c = self.salt_data.coefficients[cation]
-    nu_a = self.salt_data.coefficients[anion]
-
-    nu = nu_c + nu_a
-    i = self.get_ionic_strength(self.m)
-
-    a_phi_p1 = self.get_a_phi(self.T, self.P1)
-    a_phi_p2 = self.get_a_phi(self.T, self.P2)
-    return - abs(z_c * z_a) * (a_phi_p2 - a_phi_p1) * np.sqrt(i) / (
-            1 + 1.2 * np.sqrt(i)) \
-           + self.m * 2 * nu_c * nu_a / nu * (
-                   self.int_pbeta0p + self.int_pbeta1p * np.exp(-alpha * np.sqrt(i))
-           ) \
-           + self.m ** 2 * 4 * nu_c ** 2 * nu_a / nu * (
-                   self.int_pc0p + self.int_pc1p * np.exp(-self.alpha2 * np.sqrt(i))
-           )
+    def h(self, i):
+        b = 1.2
+        return np.log(1 + b * np.sqrt(i)) / (2 * b)
 
 
-def activity_coefficient_difference(self):
-    """
-    Returns:the difference between γ±(P2) and γ±(P1), that is, γ±(P2) - γ±(P1).
-    """
-    alpha = self.alpha
-    cation = self.salt_data.cations[0]
-    anion = self.salt_data.cations[0]
-    z_c = get_charge_number(cation)
-    z_a = get_charge_number(anion)
+    def osmotic_coefficient_difference(self):
+        """
+        Returns:the difference between φ(P2) and φ(P1), that is, φ(P2) - φ(P1).
+        """
+        alpha = self.alpha
+        cation = self.salt_data.cations[0]
+        anion = self.salt_data.cations[0]
+        z_c = get_charge_number(cation)
+        z_a = get_charge_number(anion)
 
-    nu_c = self.salt_data.coefficients[cation]
-    nu_a = self.salt_data.coefficients[anion]
+        nu_c = self.salt_data.coefficients[cation]
+        nu_a = self.salt_data.coefficients[anion]
 
-    nu = nu_c + nu_a
-    i = self.get_ionic_strength(self.m)
-    a_phi_p1 = self.get_a_phi(self.T, self.P1)
-    a_phi_p2 = self.get_a_phi(self.T, self.P2)
+        nu = nu_c + nu_a
+        i = self.get_ionic_strength(self.m)
 
-    return - abs(z_c * z_a) * (a_phi_p2 - a_phi_p1) * (
-            np.sqrt(i) / (1 + np.sqrt(i)) + 2 / 1.2 * np.log(1 + 1.2 * np.sqrt(i))
-    ) \
-           + self.m * 2 * nu_c * nu_a / nu * (
-                   2 * self.int_pbeta0p + 2 / (alpha ** 2 * i) * self.int_pbeta1p * (
-                   1 - (1 + alpha * np.sqrt(i) - alpha ** 2 * i / 2) * np.exp(-alpha * np.sqrt(i)))
-           ) \
-           + self.m ** 2 * (2 * nu_c ** 2 * nu_a * z_c) / nu * (
-                   3 * self.int_pc0p + 4 * self.int_pc1p * (
-                   6 - (6 + 6 * self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (
-                   3 / 2) - self.alpha2 ** 4 * i ** 2 / 2) * np.exp(-self.alpha2 * np.sqrt(i))
-           ) / (
-                           self.alpha2 ** 4 * i ** 2
-                   )
-           )
+        a_phi_p1 = self.get_a_phi(self.T, self.P1)
+        a_phi_p2 = self.get_a_phi(self.T, self.P2)
+        return - abs(z_c * z_a) * (a_phi_p2 - a_phi_p1) * np.sqrt(i) / (
+                1 + 1.2 * np.sqrt(i)) \
+               + self.m * 2 * nu_c * nu_a / nu * (
+                       self.int_pbeta0p + self.int_pbeta1p * np.exp(-alpha * np.sqrt(i))
+               ) \
+               + self.m ** 2 * 4 * nu_c ** 2 * nu_a / nu * (
+                       self.int_pc0p + self.int_pc1p * np.exp(-self.alpha2 * np.sqrt(i))
+               )
 
 
-def apparent_molar_enthalpy_difference(self):
-    """
-    Returns: the difference between ϕL(P2) and ϕL(P1), that is, ϕL(P2) - ϕL(P1), J·mol⁻¹.
-    """
-    cation = self.salt_data.cations[0]
-    anion = self.salt_data.cations[0]
-    z_c = get_charge_number(cation)
-    z_a = get_charge_number(anion)
+    def activity_coefficient_difference(self):
+        """
+        Returns:the difference between γ±(P2) and γ±(P1), that is, γ±(P2) - γ±(P1).
+        """
+        alpha = self.alpha
+        cation = self.salt_data.cations[0]
+        anion = self.salt_data.cations[0]
+        z_c = get_charge_number(cation)
+        z_a = get_charge_number(anion)
 
-    nu_c = self.salt_data.coefficients[cation]
-    nu_a = self.salt_data.coefficients[anion]
+        nu_c = self.salt_data.coefficients[cation]
+        nu_a = self.salt_data.coefficients[anion]
 
-    nu = nu_c + nu_a
+        nu = nu_c + nu_a
+        i = self.get_ionic_strength(self.m)
+        a_phi_p1 = self.get_a_phi(self.T, self.P1)
+        a_phi_p2 = self.get_a_phi(self.T, self.P2)
 
-    T = self.T
-    m = self.m
-    a_h_p1 = self.get_a_l(
-        T=self.T,
-        P=self.P1
-    )
-    a_h_p2 = self.get_a_l(
-        T=self.T,
-        P=self.P2
-    )
-
-    i = self.get_ionic_strength(m=m)
-
-    int_pbvt = self.int_pb0pt + 2 * self.int_pbeta1pt * (
-            1 - (1 + self.alpha * np.sqrt(i)) * np.exp(-self.alpha * np.sqrt(i))
-    ) / (self.alpha ** 2 * i)
-
-    int_pcvt = self.pc0pt + 4 * self.int_pc1pt * (
-            6 - (
-            6 + self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (3 / 2)
-    ) * np.exp(-self.alpha2 * np.sqrt(i))
-    ) / (self.alpha2 ** 4 * i ** 2)
-
-    return nu * abs(z_c * z_a) * (a_h_p2 - a_h_p1) * self.h(i=i) \
-           - 2 * nu_c * nu_a * tp.R * T ** 2 * (
-                   m * int_pbvt
-                   + m ** 2 * (nu_c * z_c) * int_pcvt
-           )
+        return - abs(z_c * z_a) * (a_phi_p2 - a_phi_p1) * (
+                np.sqrt(i) / (1 + np.sqrt(i)) + 2 / 1.2 * np.log(1 + 1.2 * np.sqrt(i))
+        ) \
+               + self.m * 2 * nu_c * nu_a / nu * (
+                       2 * self.int_pbeta0p + 2 / (alpha ** 2 * i) * self.int_pbeta1p * (
+                       1 - (1 + alpha * np.sqrt(i) - alpha ** 2 * i / 2) * np.exp(-alpha * np.sqrt(i)))
+               ) \
+               + self.m ** 2 * (2 * nu_c ** 2 * nu_a * z_c) / nu * (
+                       3 * self.int_pc0p + 4 * self.int_pc1p * (
+                       6 - (6 + 6 * self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (
+                       3 / 2) - self.alpha2 ** 4 * i ** 2 / 2) * np.exp(-self.alpha2 * np.sqrt(i))
+               ) / (
+                               self.alpha2 ** 4 * i ** 2
+                       )
+               )
 
 
-def apparent_molar_heat_capacity_difference(self):
-    """
-    Returns:the difference between ϕCp(P2) and ϕCp(P1), that is, ϕCp(P2) - ϕCp(P1).
-    """
-    cation = self.salt_data.cations[0]
-    anion = self.salt_data.cations[0]
-    z_c = get_charge_number(cation)
-    z_a = get_charge_number(anion)
+    def apparent_molar_enthalpy_difference(self):
+        """
+        Returns: the difference between ϕL(P2) and ϕL(P1), that is, ϕL(P2) - ϕL(P1), J·mol⁻¹.
+        """
+        cation = self.salt_data.cations[0]
+        anion = self.salt_data.cations[0]
+        z_c = get_charge_number(cation)
+        z_a = get_charge_number(anion)
 
-    nu_c = self.salt_data.coefficients[cation]
-    nu_a = self.salt_data.coefficients[anion]
+        nu_c = self.salt_data.coefficients[cation]
+        nu_a = self.salt_data.coefficients[anion]
 
-    nu = nu_c + nu_a
-    i = self.get_ionic_strength(self.m)
-    a_j_p1 = self.get_a_j(self.T, self.P1)
-    a_j_p2 = self.get_a_j(self.T, self.P2)
+        nu = nu_c + nu_a
 
-    int_pbvt2 = self.int_pbeta0ptt + 2 * self.int_pbeta1ptt * (
-            1 - (1 + self.alpha * np.sqrt(i)) * np.exp(-self.alpha * np.sqrt(i))
-    ) / (self.alpha ** 2 * i)
-    int_pbvt = self.int_pb0pt + 2 * self.int_pbeta1pt * (
-            1 - (1 + self.alpha * np.sqrt(i)) * np.exp(-self.alpha * np.sqrt(i))
-    ) / (self.alpha ** 2 * i)
+        T = self.T
+        m = self.m
+        a_h_p1 = self.get_a_l(
+            T=self.T,
+            P=self.P1
+        )
+        a_h_p2 = self.get_a_l(
+            T=self.T,
+            P=self.P2
+        )
 
-    int_pcvt2 = self.pc0ptt + 4 * self.int_pc1ptt * (
-            6 - (
-            6 + self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (3 / 2)
-    ) * np.exp(-self.alpha2 * np.sqrt(i))
-    ) / (self.alpha2 ** 4 * i ** 2)
+        i = self.get_ionic_strength(m=m)
 
-    int_pcvt = self.pc0pt + 4 * self.int_pc1pt * (
-            6 - (
-            6 + self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (3 / 2)
-    ) * np.exp(-self.alpha2 * np.sqrt(i))
-    ) / (self.alpha2 ** 4 * i ** 2)
+        int_pbvt = self.int_pb0pt + 2 * self.int_pbeta1pt * (
+                1 - (1 + self.alpha * np.sqrt(i)) * np.exp(-self.alpha * np.sqrt(i))
+        ) / (self.alpha ** 2 * i)
 
-    return nu * abs(z_c * z_a) * (a_j_p2 - a_j_p1) * self.h(i=i) \
-           + self.T * self.int_pv0t2 \
-           + 2 * nu_c * nu_a * tp.R * self.T ** 2 * (
-                   self.m * (int_pbvt2 + 2 / self.T * int_pbvt) +
-                   self.m ** 2 * nu_c * z_c * (int_pcvt2 + 2 / self.T * int_pcvt)
-           )
+        int_pcvt = self.pc0pt + 4 * self.int_pc1pt * (
+                6 - (
+                6 + self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (3 / 2)
+        ) * np.exp(-self.alpha2 * np.sqrt(i))
+        ) / (self.alpha2 ** 4 * i ** 2)
+
+        return nu * abs(z_c * z_a) * (a_h_p2 - a_h_p1) * self.h(i=i) \
+               - 2 * nu_c * nu_a * tp.R * T ** 2 * (
+                       m * int_pbvt
+                       + m ** 2 * (nu_c * z_c) * int_pcvt
+               )
+
+
+    def apparent_molar_heat_capacity_difference(self):
+        """
+        Returns:the difference between ϕCp(P2) and ϕCp(P1), that is, ϕCp(P2) - ϕCp(P1).
+        """
+        cation = self.salt_data.cations[0]
+        anion = self.salt_data.cations[0]
+        z_c = get_charge_number(cation)
+        z_a = get_charge_number(anion)
+
+        nu_c = self.salt_data.coefficients[cation]
+        nu_a = self.salt_data.coefficients[anion]
+
+        nu = nu_c + nu_a
+        i = self.get_ionic_strength(self.m)
+        a_j_p1 = self.get_a_j(self.T, self.P1)
+        a_j_p2 = self.get_a_j(self.T, self.P2)
+
+        int_pbvt2 = self.int_pbeta0ptt + 2 * self.int_pbeta1ptt * (
+                1 - (1 + self.alpha * np.sqrt(i)) * np.exp(-self.alpha * np.sqrt(i))
+        ) / (self.alpha ** 2 * i)
+        int_pbvt = self.int_pb0pt + 2 * self.int_pbeta1pt * (
+                1 - (1 + self.alpha * np.sqrt(i)) * np.exp(-self.alpha * np.sqrt(i))
+        ) / (self.alpha ** 2 * i)
+
+        int_pcvt2 = self.pc0ptt + 4 * self.int_pc1ptt * (
+                6 - (
+                6 + self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (3 / 2)
+        ) * np.exp(-self.alpha2 * np.sqrt(i))
+        ) / (self.alpha2 ** 4 * i ** 2)
+
+        int_pcvt = self.pc0pt + 4 * self.int_pc1pt * (
+                6 - (
+                6 + self.alpha2 * np.sqrt(i) + 3 * self.alpha2 ** 2 * i + self.alpha2 ** 3 * i ** (3 / 2)
+        ) * np.exp(-self.alpha2 * np.sqrt(i))
+        ) / (self.alpha2 ** 4 * i ** 2)
+
+        return nu * abs(z_c * z_a) * (a_j_p2 - a_j_p1) * self.h(i=i) \
+               + self.T * self.int_pv0t2 \
+               + 2 * nu_c * nu_a * tp.R * self.T ** 2 * (
+                       self.m * (int_pbvt2 + 2 / self.T * int_pbvt) +
+                       self.m ** 2 * nu_c * z_c * (int_pcvt2 + 2 / self.T * int_pcvt)
+               )
